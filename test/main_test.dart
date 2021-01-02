560fd894f673dede95368fb634550c0f95280992
Teemu Puurunen <tpuurune@gmail.com>
2021-01-02 22:43:40 +0200

    Main app tests

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,1 +1,11 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:runanonymous/main.dart';
+
+void main() {
+  testWidgets('RunanonymousApp has a MaterialApp', (WidgetTester tester) async {
+    await tester.pumpWidget(RunanonymousApp());
+    final materialAppFinder = find.byType(MaterialApp);
+    expect((materialAppFinder), findsOneWidget);
+  });
+}
